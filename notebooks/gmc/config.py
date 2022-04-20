c9ffa92050a46e7249eea84c295455b942269e85
Simon Helmig <40271543+helmigsimon@users.noreply.github.com>
2022-04-20 07:28:08 +0200

    Initialized project according to cookiecutter

--- notebooks/gmc/config.py
+++
@@ -1,9 +0,0 @@
-import os
-import pathlib
-
-CONFIG_PATH = pathlib.Path(os.path.abspath(__name__))
-GMC_PATH = CONFIG_PATH.parent
-ROOT = GMC_PATH.parent
-DATA_PATH = ROOT / "data"
-INPUT_DATA_PATH = DATA_PATH/'input'
-
--- notebooks/gmc/setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup
-
-setup(name='gmc',
-      version='0.1',
-      description='Give Me Some Credit Package',
-      author='Simon Helmig',
-     )
